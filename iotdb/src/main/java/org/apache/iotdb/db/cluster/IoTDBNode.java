7d3febe3b61b11492bb557157b6ba5264e74c14e
lta <litiananfa@163.com>
2019-03-04 10:43:40 +0800

    add akka cluster example

---
+++ iotdb/src/main/java/org/apache/iotdb/db/cluster/IoTDBNode.java
@@ -0,0 +1,66 @@
+package org.apache.iotdb.db.cluster;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.cluster.ClusterEvent.MemberEvent;
+import akka.cluster.ClusterEvent.MemberRemoved;
+import akka.cluster.ClusterEvent.MemberUp;
+import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import org.apache.iotdb.db.exception.FileNodeManagerException;
+import org.apache.iotdb.db.service.IoTDB;
+
+/**
+ * @author lta
+ */
+public class IoTDBNode extends AbstractActor {
+
+  private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+  private Cluster cluster = Cluster.get(getContext().system());
+  private IoTDB daemon;
+
+  static public Props props() {
+    return Props.create(IoTDBNode.class, () -> new IoTDBNode());
+  }
+
+  //subscribe to cluster changes
+  @Override
+  public void preStart() {
+    cluster.subscribe(self(), ClusterEvent.initialStateAsEvents(),
+        MemberEvent.class, UnreachableMember.class);
+    daemon = IoTDB.getInstance();
+    daemon.active();
+  }
+
+  //re-subscribe when restart
+  @Override
+  public void postStop() throws FileNodeManagerException {
+    cluster.unsubscribe(self());
+    daemon.stop();
+  }
+
+  public IoTDBNode() {
+  }
+
+
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+        .match(MemberUp.class, mUp -> {
+          log.info("IoTDB node is Up: {}", mUp.member());
+        })
+        .match(UnreachableMember.class, mUnreachable -> {
+          log.info("IoTDB node detected as unreachable: {}", mUnreachable.member());
+        })
+        .match(MemberRemoved.class, mRemoved -> {
+          log.info("IoTDB node is Removed: {}", mRemoved.member());
+        })
+        .match(MemberEvent.class, message -> {
+          // ignore
+        })
+        .build();
+  }
+}
